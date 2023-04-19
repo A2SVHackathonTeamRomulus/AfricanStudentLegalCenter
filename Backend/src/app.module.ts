56a6b9903ac577d8fa3c71784a87c4b65e55c2bd
Biniyam Seid <Biniyamseid3@gmail.com>
2023-04-19 10:42:18 +0300

    crud on blog

--- Backend/src/app.module.ts
+++ Backend/src/app.module.ts
@@ -3,10 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { BlogService } from './blog/blog.service';
+import { BlogController } from './blog/blog.controller';
 
 @Module({
   imports: [AuthModule, PrismaModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, BlogController],
+  providers: [AppService, BlogService],
 })
 export class AppModule {}
